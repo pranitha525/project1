d8d1c104b75432892ad6afd91bf8f317623d2a87
pranitha525 <pranitha.kyatham25@gmail.com>
2020-02-07 22:00:17 +0530

    first commit

---
+++ src/test/java/com/git/tests/Testsuite2.java
@@ -0,0 +1,14 @@
+package com.git.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testsuite2 {
+
+	@Test
+	public void test() {
+		System.out.println("test2");
+	}
+
+}
---
+++ src/test/java/com/git/tests/Testsuite3.java
@@ -0,0 +1,14 @@
+package com.git.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testsuite3 {
+
+	@Test
+	public void test() {
+		System.out.println("test3");
+	}
+
+}
